f5e243cca297c383d9dcfddfa64df45641fc9da4
Carson Gee <carson.gee@datarobot.com>
2023-01-30 22:03:29 -0700

    Flakiness without approval;

--- component/tests/unit/test_flaky.py
+++ component/tests/unit/test_flaky.py
@@ -2,11 +2,11 @@ import random
 
 import pytest
 
-IS_FLAKY = False
+IS_FLAKY = True
 
 
 @pytest.mark.parametrize("param", range(0, 90_000))
 def test_flaky(param):
     assert param < 100_000
     if IS_FLAKY:
-        assert random.randint(0, 1_000)
+        assert random.randint(0, 100)
